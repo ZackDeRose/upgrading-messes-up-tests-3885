9e0b742df325b30917e8797d409bf8e726bbc2cb
Zack DeRose <zack.derose@gmail.com>
2021-04-09 15:43:53 -0700

    manually fixing again (test now pass)

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  passWithNoTests: true,
-  projects: '<rootDir>/libs/another-lib-after-migrating',
+  passWithNoTests: true
 };
